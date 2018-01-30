90cef58368c6279171099aa722722cdf79923bc5
Tharuka <tharudg1@gmail.com>
2018-01-30 16:56:25 +0530

    navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 
 import {SignInComponent} from './components/sign-in/sign-in.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 
 
@@ -10,6 +11,10 @@ export const AppRoutes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent,
+  },
+  {
+    path: 'about-us',
+    component: ContactUsComponent,
   },
   { path: '**',
     component: PageNotFoundComponent
